eceb550be8a741ebd71ab629043560cbb759906c
Oğuzhan DERECİ <oguzz.dereci@gmail.com>
2019-01-07 11:28:43 +0300

    Proje katmanları oluşturuldu

---
+++ MarketOtomasyon.DAL/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOtomasyon.DAL
+{
+    public class MyContext:DbContext
+    {
+        public MyContext()
+        {
+
+        }
+    }
+}
