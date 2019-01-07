88bc234b4191f912e49c20c0ab6b526e31be6462
Emre Çetin <emre_17_cetin@hotmail.com>
2019-01-07 12:06:33 +0300

    SaleDetail,Package

--- MarketOtomasyon.Models/Entities/Package.cs
+++ MarketOtomasyon.Models/Entities/Package.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using MarketOtomasyon.Models.Abstracts;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MarketOtomasyon.Models.Entities
 {
-    class Package
+    [Table("Packages")]
+    public class Package : BaseEntity<Guid>
     {
+        public Package()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        public int KoliAdedi { get; set; }
+        public int Barkod { get; set; }
+
+
+    //     [ForeignKey("ProductId")]
+    //    public virtual Product Product { get; set; }
     }
+
 }
