33bb3f895362b85fda2a63b63d24d690d738ae3b
Bugra Kırman <bugrakirman@gmail.com>
2019-01-07 11:46:30 +0300

    order orderdetails tabloları eklendi

--- MarketOtomasyon.Models/Entities/Order.cs
+++ MarketOtomasyon.Models/Entities/Order.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using MarketOtomasyon.Models.Abstracts;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MarketOtomasyon.Models.Entities
 {
-    class Order
+    [Table("Order")]
+    public class Order: BaseEntity<Guid>
     {
+        public Order()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        [StringLength(50)]
+        public string Category { get; set; }
+        [StringLength(50)]
+        public string ProductName { get; set; }
+        public int PackageQuantity { get; set; }
     }
 }
--- MarketOtomasyon.Models/Entities/OrderDetail.cs
+++ MarketOtomasyon.Models/Entities/OrderDetail.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using MarketOtomasyon.Models.Abstracts;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MarketOtomasyon.Models.Entities
 {
-    class OrderDetail
+    [Table("OrderDetails")]
+    public class OrderDetail: BaseEntity<Guid>
     {
+        public OrderDetail()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        [StringLength(50)]
+        public string ProductName { get; set; }
+        [StringLength(50)]
+        public string Category { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int PackageQuantity { get; set; }
     }
 }
