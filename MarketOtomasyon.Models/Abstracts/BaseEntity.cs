5252c77632bab936cf1dbd050d9a1d996bc59d7a
Oğuzhan DERECİ <oguzz.dereci@gmail.com>
2019-01-07 11:38:23 +0300

    BaseEntity sınıfı eklendi

---
+++ MarketOtomasyon.Models/Abstracts/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOtomasyon.Models.Abstracts
+{
+    public abstract class BaseEntity<T>
+    {
+        [Key]
+        [Column(Order = 1)]
+        public T Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
