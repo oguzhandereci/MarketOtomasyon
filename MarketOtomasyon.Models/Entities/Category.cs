c747abd31d74ac507dc38b2c18402e669a7cbc6a
Oğuzhan DERECİ <oguzz.dereci@gmail.com>
2019-01-07 12:03:56 +0300

    Category ve Product classları duzenlendi

--- MarketOtomasyon.Models/Entities/Category.cs
+++ MarketOtomasyon.Models/Entities/Category.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using MarketOtomasyon.Models.Abstracts;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MarketOtomasyon.Models.Entities
 {
-    class Category
+    [Table("Categories")]
+    public class Category:BaseEntity<Guid>
     {
+        [Required]
+        [StringLength(120)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? SupCategoryId { get; set; }
+        public decimal KdvOrani { get; set; }
+
+
+        //[ForeignKey("SupCategoryId")]
+        //public virtual Category SupCategory { get; set; }
+        //public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+        //public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
     }
 }
--- MarketOtomasyon.Models/Entities/Product.cs
+++ MarketOtomasyon.Models/Entities/Product.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using MarketOtomasyon.Models.Abstracts;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MarketOtomasyon.Models.Entities
 {
-    public class Product
+    [Table("Products")]
+    public class Product:BaseEntity<Guid>
     {
+        public Product()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [StringLength(100)]
+        [Required]
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public decimal BuyPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public int CategoryId { get; set; }
+        public decimal StockQuantity { get; set; }
+
+
+        //[ForeignKey("CategoryId")]
+        //public virtual Category Category { get; set; }
     }
 }
