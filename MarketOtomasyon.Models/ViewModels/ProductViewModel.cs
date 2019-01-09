23e6e4efeb6983c1e0fc2fee09217dc809c49a09
Bugra Kırman <bugrakirman@gmail.com>
2019-01-09 11:17:10 +0300

    productviewmodel eklendi

---
+++ MarketOtomasyon.Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOtomasyon.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public string Barcode { get; set; }
+        public decimal SellPrice { get; set; }
+        public decimal? StockQuantity { get; set; }
+
+        public override string ToString() => $"{ProductName} - {SellPrice:c2} - {StockQuantity}";
+    }
+}
--- MarketOtomasyon/FrmUrunEkle.cs
+++ MarketOtomasyon/FrmUrunEkle.cs
@@ -21,7 +21,7 @@ namespace MarketOtomasyon
             InitializeComponent();
         }
 
-        
+
 
         private void btnAddCategory_Click(object sender, EventArgs e)
         {
@@ -60,7 +60,7 @@ namespace MarketOtomasyon
                         throw new Exception("Bu isimde bir kategori bulunmaktadir");
                 }
 
-                
+
             }
             catch (Exception ex)
             {
@@ -71,7 +71,7 @@ namespace MarketOtomasyon
         }
         private void btnAddProduct_Click(object sender, EventArgs e)
         {
-            if (txtProduct.Text == null || cmbCategory.SelectedItem == null || txtBarcode.Text==null || txtSellPrice.Text==null) return;
+            if (txtProduct.Text == null || cmbCategory.SelectedItem == null || txtBarcode.Text == null || txtSellPrice.Text == null) return;
 
             List<Product> products = new ProductRepo().GetAll();
             try
@@ -83,7 +83,7 @@ namespace MarketOtomasyon
                     Barcode = txtBarcode.Text,
                     SellPrice = Convert.ToDecimal(txtSellPrice.Text)
                 };
-                
+
                 using (var productRepo = new ProductRepo())
                 {
                     foreach (var item in products)
@@ -120,9 +120,19 @@ namespace MarketOtomasyon
         private void FrmUrunEkle_Load_1(object sender, EventArgs e)
         {
             List<CmbCategoryViewModel> categories = new List<CmbCategoryViewModel>();
-            List<Product> products = new ProductRepo().GetAll();
+            List<ProductViewModel> products = new List<ProductViewModel>();
             try
             {
+                products.AddRange(new ProductRepo().GetAll()
+                    .OrderBy(x => x.ProductName)
+                    .Select(x => new ProductViewModel()
+                    {
+                        Id = x.Id,
+                        ProductName = x.ProductName,
+                        SellPrice = x.SellPrice,
+                        StockQuantity = x.StockQuantity,
+                        Barcode = x.Barcode
+                    }));
                 categories.AddRange(new CategoryRepo().GetAll()
                     .OrderBy(x => x.CategoryName)
                     .Select(x => new CmbCategoryViewModel()
