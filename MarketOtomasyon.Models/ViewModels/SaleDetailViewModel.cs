15a9951157dc76c4fa91dddfce1e3f0368400ffa
Emre Çetin <cetinemre17@gmail.com>
2019-01-14 22:07:37 +0300

    Basit şekilde pdf oluşturuldu

---
+++ MarketOtomasyon.Models/ViewModels/SaleDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOtomasyon.Models.ViewModels
+{
+    public class SaleDetailViewModel
+    {
+        public int id { get; set; }
+        public Guid id2 { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public override string ToString() => $" {Quantity}  *  {ProductName} = {TotalPrice:c2}";
+    }
+}
